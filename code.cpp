a8bee9909835061cbe802eaff122c534d011a31a
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-23 20:40:18 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,12 @@
+//Prompt the user to enter his/her university Name : 
+#include <iostream>
+using namespace std;
+int main (){
+	
+	string uniname;
+	cout << "Enter your University name : " ;
+	getline (cin, uniname);
+	cout << "Your University is " << uniname << endl;
+	
+	return 0;
+}
